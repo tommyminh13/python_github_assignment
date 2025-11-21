9c92e7cb40de45e3e70a6bce69790eed4edd52c4
Minh Thai <minhth070513@gmail.com>
2025-11-20 19:18:16 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my Python program!")
 hours = input("How many hours did you study today? ")
 hours = float(hours)
 weekly_hours = hours * 7
+print(f"You are on track to study {weekly_hours} hours this week.")
