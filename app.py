893081c3beed4431df204e0698e19b37703c17e5
Minh Thai <minhth070513@gmail.com>
2025-11-23 23:49:24 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ hours = input("How many hours do you plan to go to the gym today? ")
 
 hours = float(hours)
 weekly_hours = hours * 7
+
+print(f"\nIf you keep this pace, you will spend {weekly_hours} hours at the gym this week!")
